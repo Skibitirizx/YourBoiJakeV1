9186640558484526433f2b88af18e0d4654e10f0
Skibitirizx <jacobcox6689@gmail.com>
2024-12-06 01:41:14 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -25,7 +25,7 @@ var games = [
     image: 'https://th.bing.com/th?id=OIP.6jjUQ7Niv2cl4NvkOrkf_QAAAA&w=255&h=244&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2'
   },
   {
-    id: 'capybara clicker',
+    id: 'capybaraclicker',
     title: 'Capybara Clicker',
     url: 'https://totallyscience.co/games/2024/onl/capybara-clicker/index.html',
     image: 'https://totallyscience.co/media/posts/733/responsive/Capybara-Clicker-xs.jpg'
